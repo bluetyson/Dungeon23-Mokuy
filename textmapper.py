409c43c70de8ea37380b4a875216403708688cc0
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-31 16:32:47 +1030

    hr

--- textmapper.py
+++ textmapper.py
@@ -1,10 +1,18 @@
 def hr(start, stop, base):
     uselist = []
-    for i in range(stop-start):
-        usestr = str(i+start) + str(base)
+    r = stop - start + 1
+    for idx in range(r):
+        if len(base) == 1:
+            base = "0" + str(base)
+        if len(idx+start) == 1:
+            idxstr = "0" + str(idx+start)            
+        else: 
+            idxstr = str(idx+start)
+
+        usestr = str(idxstr) + str(base)
         uselist.append(usestr)
         #print(usestr)
-        return(uselist)
+    return(uselist)
 
 terrain = {}
 terrain['sand'] = ["0710","0711","0712","0713","0714","0716","0810","0817","0818","0819","0820","0909","0920",'1008','1009']
@@ -14,9 +22,9 @@ terrain['desert'] = ['0716','0813']
 terrain['mountain'] = ['1011']
 terrain['river'] = ['1306','1307']
 terrain['water'] = ['2010','2110','2111','2211'] #inlandsea
-terrain['bush'] = ['0811'] #bush1
+terrain['bush'] = ['0811','1110']+hr(11,12,8)+hr(12,13,9)
 terrain['trees'] = ['1217']
-terrain['forest'] = hr(11,18,16)+hr(19,21,17)+hr(23,24,17)+hr(25,25,17)+hr(21,22,18)+hr(25,25,19)++hr(26,20,20)
+terrain['forest'] = hr(11,18,16)+hr(19,21,17)+hr(23,24,17)+hr(25,25,17)+hr(21,22,18)+hr(25,25,19)+hr(26,20,20)
 terrain['volcano'] = ['3417'] #lord howe
 terrain['fir'] = ['3513'] #norfolk
 
