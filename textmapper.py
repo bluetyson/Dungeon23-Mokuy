c6afe8863a5be97a5e2db58ab747690c4770a661
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-31 15:46:46 +1030

    Create textmapper.py

---
+++ textmapper.py
@@ -0,0 +1,5 @@
+terrain = {}
+terrain['sand'] = ["0710","0711","0712","0713","0714","0716"]+ ["0810","0817","0818","0819","0820"]+ ["0909","0920"]
+terrain['sand'] = terrain['sand'] 
+terrain['sand'] = terrain['sand'] 
+terrain['pine'] = ['3513']
